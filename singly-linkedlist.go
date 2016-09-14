e32384d5b19ca832a6f96f6501ad902a3bc73d5c
ranjith-mrk <innranjith@gmail.com>
2016-09-15 01:03:20 +0530

    Proper structured format

--- singly-linkedlist.go
+++ singly-linkedlist.go
@@ -1,4 +1,6 @@
-package list
+package main
+
+import "fmt"
 
 type Element struct {
   next *Element
@@ -11,14 +13,30 @@ type List struct {
   len int
 }
 
-func (e *Element) next() {
-  if n := e.next(); e.list != nil && n != &e.list.root{
+func (e *Element) Next() *Element{
+  if n := e.next; e.list != nil && n != &e.list.root{
     return n
   }
   return nil
 }
 
+func (l *List) init() *List{
+  l.root.next = &l.root
+  l.len = 0
+  return l
+}
+
+func (l *List) insert(e, at *Element) *List {
+  n := at.Next()
+  at.next = e
+  e.next = n
+  e.list = l 
+  l.len = l.len + 1
+  return l
+}
+
 
 func main() {
-  
+  l := new(List).init()
+  fmt.Println(l)
 }
