860125c7a08f5cc188acdda976c99f08de8cf0b4
ranjith-mrk <innranjith@gmail.com>
2016-09-16 01:15:02 +0530

    Proper working singly linked list

--- singly-linkedlist.go
+++ singly-linkedlist.go
@@ -26,17 +26,26 @@ func (l *List) init() *List{
   return l
 }
 
-func (l *List) insert(e, at *Element) *List {
+func (l *List) insert(e, at *Element) *Element {
   n := at.Next()
   at.next = e
   e.next = n
   e.list = l 
   l.len = l.len + 1
-  return l
+  return e
 }
 
+func (l *List) insertValue(v interface{}, at *Element) *Element {
+  return l.insert(&Element{Value: v}, at)
+}
+
+func New() *List { return new(List).init() }
 
 func main() {
-  l := new(List).init()
-  fmt.Println(l)
+  l := New()
+  e := l.insertValue(1, l.root.next)
+  e1 := l.insertValue(3, e)
+  e2 := l.insertValue(2, e1)
+  fmt.Println(e2)
+  fmt.Println(l.root.next.next.next.Value)
 }
